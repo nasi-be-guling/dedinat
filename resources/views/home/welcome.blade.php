bc6861479b7be054bfbd8c8790c1cb0a7391339a
putu@ <putu@poltekkesdepkes-sby.ac.id>
2025-06-05 09:39:41 +0700

    melakukan perubahan tampilan homepage

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -28,6 +28,17 @@
                     </div>
                 </div>
             </div>
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h4 class="card-title">Penjelasan</h4>
+                    <ol>
+                        <li>Instrumen DEDI NATANIEL ini bertujuan membantu saudara melakukan deteksi dini secara mandiri apabila menemukan masalah masalah perkembangan yang berbeda dengan perkembangan anak normal seusianya dan dengan menemukan kelainan/perbedaan yang terjadi pada putra/putri saudara secara dini maka akan segera mendapatkan intervensi dini dan tindak lajut secara tepat dan benar.</li>
+                        <li>Jawablah dengan jujur kelainan/perbedaan yang saudara rasakan pada anak melalui pengamatan, Observasi, Wawancara, saat memberi perintah pada anak atau gabungan dari (pengamatan, Observasi, Wawancara)</li>
+                        <li>Apabila menemukan kelainan/perbedaan yang tidak normal pada anak, tunjukan instrument yang telah saudara isi dan segera konsultasi dengan tenaga ahli (SLB) atau bawa anak ke fasilitas kesehatan</li>
+                    </ol>
+                </div>
+            </div>
+
             <div class="col-sm-12 text-center mt-4">
                 <a type="button" class="btn btn-md btn-primary waves-effect waves-light col-sm-3" style="color: white;"
                     href="{{ route('home.start') }}">Mulai</a>
